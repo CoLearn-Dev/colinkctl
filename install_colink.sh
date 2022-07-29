c8c042b6f49f7886ab8c7551f2cab633b2253a82
stneng <git@stneng.com>
2022-07-29 02:46:07 +0000

    .

--- install_colink.sh
+++ install_colink.sh
@@ -8,7 +8,7 @@ mkdir -p $COLINK_HOME
 
 PAYLOAD_LINE=$(awk '/^__PAYLOAD_BEGINS__/ { print NR + 1; exit 0; }' $0)
 
-# tail -n +${PAYLOAD_LINE} $0 | tar -px -C $COLINK_HOME
+tail -n +${PAYLOAD_LINE} $0 | tar -px -C $COLINK_HOME
 
 $COLINK_HOME/colinkctl install
 
